2d3afa053801ec7823f75491d4ee3f23526097a8
bdrackley <brendandrackley@gmail.com>
2018-01-22 15:28:57 -0600

    added licensing info

--- README.md
+++
@@ -1,2 +0,0 @@
-# VASIC_Qt_GUI
-VASIC GUI created using the Qt framework
--- calibrationwindow.cpp
+++ calibrationwindow.cpp
@@ -1,3 +1,32 @@
+/****************************************************************************
+**
+** VASIC Data Acquisition GUI - a VASIC GUI created using the Qt framework
+**
+** Copyright (C) (2018) Yang Laboratory, University of Wisconsin - Madison
+**
+** Distributed under the GNU Public License (GPL)
+** See the file 'COPYING' for details
+**
+** Author: Brendan Drackley
+** Email: brendandrackley@gmail.com
+**
+** Please send bug reports to: jyang75@wisc.edu
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
 #include "calibrationwindow.h"
 #include "ui_calibrationwindow.h"
 #include <QMessageBox>
